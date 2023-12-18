ba9d0261ad0e074feb75b8a5fc627d0b11023be0
ryantran309 <s3821266@rmit.edu.vn>
2023-12-18 11:52:44 +0700

    Update SecretKeyGuesser.java

--- SecretKeyGuesser.java
+++ SecretKeyGuesser.java
@@ -1,55 +1,132 @@
-
-
 public class SecretKeyGuesser {
-  public void start() {
-    // brute force key guessing
-    SecretKey key = new SecretKey();
-    String str = "MMMMMMMMMMMM";
-    while (key.guess(str) != 12) {
-      str = next(str);
-      System.out.println("Guessing... " + str);
+
+    private SecretKey secretKey;
+
+    // Contains the guessing frequencies of the characters
+    // 0: M, 1: O, 2: C, 3: H, 4: A
+    private final int[] frequencies;
+
+    private int guessAttempt;
+    private int correctness;
+
+    // should be a char array to make it easier to handle with
+    private char highestFrequencyChar;
+    private char[] resultSecret;
+
+    // why choose "switch" over "if"? because it iz blazingly(TM) fast
+    private static char getChar(int pos) {
+        char rs = '\0';
+        switch (pos) {
+            case 0: rs = 'M'; break;
+            case 1: rs = 'O'; break;
+            case 2: rs = 'C'; break;
+            case 3: rs = 'H'; break;
+            case 4: rs = 'A'; break;
+        };
+        return rs;
     }
-    System.out.println("I found the secret key. It is " + str);
-  }
 
-  static int order(char c) {
-    if (c == 'M') {
-      return 0;
-    } else if (c == 'O') {
-      return 1;
-    } else if (c == 'C') {
-      return 2;
-    } else if (c == 'H') {
-      return 3;
-    } 
-    return 4;
-  }
-
-  static char charOf(int order) {
-    if (order == 0) {
-      return 'M';
-    } else if (order == 1) {
-      return 'O';
-    } else if (order == 2) {
-      return 'C';
-    } else if (order == 3) {
-      return 'H';
-    } 
-    return 'A';
-  }
-
-  // return the next value in 'MOCHA' order, that is
-  // M < O < C < H < A
-  public String next(String current) {
-    char[] curr = current.toCharArray();
-    for (int i = curr.length - 1; i >=0; i--) {
-      if (order(curr[i]) < 4) {
-        // increase this one and stop
-        curr[i] = charOf(order(curr[i]) + 1);
-        break;
-      }
-      curr[i] = 'M';
+    private static int getPosition(char c) {
+        int rs = -1;
+        switch (c) {
+            case 'M': rs = 0; break;
+            case 'O': rs = 1; break;
+            case 'C': rs = 2; break;
+            case 'H': rs = 3; break;
+            case 'A': rs = 4; break;
+        }
+        return rs;
     }
-    return String.valueOf(curr);
-  }  
+
+    public SecretKeyGuesser() {
+        this.secretKey = new SecretKey();
+        this.frequencies = new int[5];
+        this.guessAttempt = 0;
+        this.highestFrequencyChar = 'M';
+        this.correctness = 0;
+    }
+
+    public void start() {
+
+        int highestFrequency = 0;
+
+        // 1. Attempt to get frequencies of characters
+        for (int i = 0; i < 5; i++) {
+            var singletonString = String.valueOf(getChar(i)).repeat(12);
+            var freq = secretKey.guess(singletonString);
+
+            guessAttempt += 1;
+            frequencies[i] = freq;
+
+            if (freq >= highestFrequency) {
+                highestFrequency = freq;
+                highestFrequencyChar = getChar(i);
+            }
+        }
+
+        // 2. after gotten the frequencies of the strings, we create a singleton string of "highest frequencies" char.
+        resultSecret = String.valueOf(highestFrequencyChar).repeat(12).toCharArray();
+        correctness = highestFrequency;
+
+        // 3. then, we guess in order, M -> O -> C -> H -> A, skip it was a "highestFrequencyChar" or non-existing chars
+        for (int i = 0; i < 5; i++) {
+            var guessingChar = getChar(i);
+            if (guessingChar == highestFrequencyChar || frequencies[i] == 0) {
+                continue;
+            }
+
+            System.out.println("Now testing: " + guessingChar);
+            attemptChar(guessingChar);
+            System.out.println();
+        }
+    }
+
+    private void attemptChar(char guessingChar) {
+
+        int charLeft = frequencies[getPosition(guessingChar)];
+
+        // we place a char into "placeable" position (which has highestFrequencyChar)
+        for (int i = 0; i < 12; i++) {
+
+            if (resultSecret[i] != highestFrequencyChar) {
+                continue;
+            }
+
+            // place the guessing char into and then test whether placement is correct
+            resultSecret[i] = guessingChar;
+            guessAttempt += 1;
+
+            var testingCorrectness = secretKey.guess(getResultSecret());
+
+            if (testingCorrectness > correctness) {
+                correctness = testingCorrectness;
+                charLeft -= 1;
+                if (charLeft == 0) {
+                    System.out.println("Temporary result secret: " + getResultSecret()
+                            + ", with correctness: "  + correctness
+                            + ", after guess attempt: " + guessAttempt);
+                    return;
+                }
+                continue;
+            }
+
+            // failed test: remove guessing char
+            resultSecret[i] = highestFrequencyChar;
+        }
+    }
+
+    public int getGuessAttempt() {
+        return guessAttempt;
+    }
+
+    public String getResultSecret() {
+        return new String(resultSecret);
+    }
+
+    public static void main(String[] args) {
+        var secretKeyGuesser = new SecretKeyGuesser();
+        secretKeyGuesser.start();
+
+        System.out.println(secretKeyGuesser.getResultSecret() + " after: " + secretKeyGuesser.getGuessAttempt());
+    }
 }
